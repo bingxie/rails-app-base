fa79278bcb36a1891b2278ccdce87f8208ef94fa
Bing Xie <bing708@gmail.com>
2015-11-09 15:31:00 +1100

    Add movie partial

--- db/migrate/20151109040909_create_genres_movies.rb
+++ db/migrate/20151109040909_create_genres_movies.rb
@@ -1,7 +1,7 @@
 class CreateGenresMovies < ActiveRecord::Migration
   def change
     create_table :genres_movies do |t|
-      t.integer :movie_id, index: true
+      t.integer :movie_id, index: true, unique: true
       t.integer :genre_id, index: true
     end
   end
