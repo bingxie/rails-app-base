2299fea4b37a3ebce04d37eb5a66518794281013
Bing Xie <bing708@gmail.com>
2015-11-09 15:11:27 +1100

    Add move and genre model

---
+++ db/migrate/20151109040909_create_genres_movies.rb
@@ -0,0 +1,8 @@
+class CreateGenresMovies < ActiveRecord::Migration
+  def change
+    create_table :genres_movies do |t|
+      t.integer :movie_id, index: true
+      t.integer :genre_id, index: true
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151109040909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "genres", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres_movies", force: :cascade do |t|
+    t.integer "movie_id"
+    t.integer "genre_id"
+  end
+
+  add_index "genres_movies", ["genre_id"], name: "index_genres_movies_on_genre_id", using: :btree
+  add_index "genres_movies", ["movie_id"], name: "index_genres_movies_on_movie_id", using: :btree
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "name"
+    t.text     "synopsis"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
