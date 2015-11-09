e85b24b6974ec3f201da329a0788b2b099200da4
Bing Xie <bing708@gmail.com>
2015-11-09 16:14:55 +1100

    Movie can raise exception

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -7,12 +7,21 @@ class Movie < ActiveRecord::Base
   has_and_belongs_to_many :genres
   has_and_belongs_to_many :crews
 
-  def add_many(type, data)
-    type.in? ['Genre', 'Crew']
-    type_plural = type.downcase.pluralize
+  class RelationError < StandardError
+    def initialize(msg = "That Relationship Type doesn't exist")
+      super(msg)
+    end
+  end
 
-    self.send("#{type_plural}=", data.map do |g|
-      type.classify.constantize.where(name: g).first_or_create!
-    end)
+  def add_many(type, data)
+    if type.in? ['Genre', 'Crew']
+      type_plural = type.downcase.pluralize
+
+      self.send("#{type_plural}=", data.map do |g|
+        type.classify.constantize.where(name: g).first_or_create!
+      end)
+    else
+      raise RelationError
+    end
   end
 end
