8031571d869c37521fe3925cd9a1a92cad960ba4
Bing Xie <bing708@gmail.com>
2015-11-11 11:00:32 +1100

    Finish the basic search function

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,44 @@
 class Article < ActiveRecord::Base
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
+
+  def self.search(query)
+    __elasticsearch__.search(
+      {
+        query: {
+          multi_match: {
+            query: query,
+            fields: ['title^10', 'text']
+          }
+        },
+        highlight: {
+          pre_tags: ['<em>'],
+          post_tags: ['</em>'],
+          fields: {
+            title: {},
+            text: {}
+          }
+        }
+      }
+    )
+  end
+
+  settings index: { number_of_shards: 1 } do
+    mappings dynamic: 'false' do
+      indexes :title, analyzer: 'ik_smart', index_options: 'offsets'
+      indexes :text, analyzer: 'ik_smart'
+    end
+  end
 end
 
-# Article.import # for auto sync model with elastic search
+# Article.__elasticsearch__.create_index! force: true
+
+# 1.7.0
+# Delete the previous articles index in Elasticsearch
+# Article.__elasticsearch__.client.indices.delete index: Article.index_name rescue nil
+
+# Create the new index with the new mapping
+# Article.__elasticsearch__.client.indices.create index: Article.index_name, body: { settings: Article.settings.to_hash, mappings: Article.mappings.to_hash }
+
+# Index all article records from the DB to Elasticsearch
+# Article.import
